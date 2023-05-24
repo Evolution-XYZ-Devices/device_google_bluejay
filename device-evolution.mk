dbd8809315b4bec02769ad3d80497da75fe753fe
AnierinB <anierin@evolution-x.org>
2023-05-24 15:49:08 -0700

    bluejay: Nuke HbmSVManager
    We will be using our own HBM/AutoHBM impl
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device-evolution.mk
+++ device-evolution.mk
@@ -15,10 +15,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
 
-# HBM
-PRODUCT_PACKAGES := \
-    HbmSVManagerOverlay
-
 # Build necessary packages for vendor
 
 # Audio
